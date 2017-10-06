d30d1b1b3043806c630b80acae06fe1dc6624f9b
naveenpiedy <nav.bugs.thepoet@gmail.com>
2017-10-05 19:29:56 -0700

    Log in module part 1

--- mainsite/mainsite/settings.py
+++ mainsite/mainsite/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'mainsite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -118,3 +118,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+LOGIN_REDIRECT_URL = 'home'
+
+STATICFILES_DIRS = [os.path.join(BASE_DIR, '/tempelates/static'), '/templates/static']
+    # Put strings here, like "/home/html/static" or "C:/www/django/static".
+    # Always use forward slashes, even on Windows.
+    # Don't forget to use absolute paths, not relative paths.
+    #os.path.join(BASE_DIR, 'static/'),
+
