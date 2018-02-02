11b022420689210e18c38fdfc43174f56c74ab2d
Naveen Piedy <naveenpiedy@users.noreply.github.com>
2018-02-02 12:57:36 -0700

    Expanded Admin functionality to show Name, email Id and all the other relevant details in a table.

--- mainsite/signupapp/admin.py
+++ mainsite/signupapp/admin.py
@@ -1,8 +1,24 @@
-from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from django.contrib.auth.models import User
 from .models import UserTable
 
+from django.contrib import admin
+
+
 # Register your models here.
 
-admin.site.register(UserTable)
+class UserTableAdmin(admin.ModelAdmin):
+    list_display = ['first_name', 'last_name', 'email', 'user', 'isprofessor', 'university']
+
+    def first_name(self, obj):
+        return obj.user.first_name
+
+    def last_name(self, obj):
+        return obj.user.last_name
+
+    def email(self, obj):
+        return obj.user.email
+
+#admin.site.register(UserTable)
+
+admin.site.register(UserTable, UserTableAdmin)
