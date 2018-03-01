1fe70801d43142dd04a9520b997bf783ae46ce5e
Chauncey <gottingen999@gmail.com>
2018-03-01 08:25:03 +0800

    Task #76 Unique Token Generation & #77 Sending reset Email Status: In progress Description: To do the "Forget Password" Module, we have 3 steps(requests) I just finished the back-end of Request_1. Also, I started the Token-Generation and Email-Sending step.

--- mainsite/loginapp/urls.py
+++ mainsite/loginapp/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import url
+from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('forget_step1', views.forget_password_step1,name='forget_password_step1'),
     url(r'^$', views.index, name='index'),
 ]
--- mainsite/loginapp/views.py
+++ mainsite/loginapp/views.py
@@ -2,6 +2,13 @@ from django.shortcuts import render
 from django.contrib.auth import authenticate, login
 from django import forms
 from django.template.context_processors import csrf
+from django.core.mail import EmailMessage
+from django.contrib.sites.shortcuts import get_current_site
+from django.template.loader import render_to_string
+from .tokens import password_reset_token
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
+from django.utils.encoding import force_bytes, force_text
+from django.http import HttpResponse
 
 
 def index(request):
@@ -21,3 +28,22 @@ def index(request):
                 print("Debugging")
     return render(request, 'loginapp/base.html', c)
 
+def forget_password_step1(request):
+    c = {}
+    c.update(csrf(request))
+    return render(request,'loginapp/step1.html',c)
+
+def forget_password_step2(request):
+    if request.method=='POST' and "button_click" in request.POST:
+        email=request.POST['email']
+        current_site = get_current_site(request)
+        message = render_to_string('loginapp/reset_email.html', {
+            'domain': current_site.domain,
+            'token': password_reset_token.make_token(),
+        })
+        mail_subject = 'Activate your account.'
+        to_email = email
+        email_obj = EmailMessage(mail_subject, message, to=[to_email])
+        email_obj.send()
+        return HttpResponse('Please confirm your email address to complete the registration')
+    return render(request,'')
