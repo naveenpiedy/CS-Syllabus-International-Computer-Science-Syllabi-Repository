9cdad7aeaa9d95b852c089ffaffda53927e781ee
Naveen Piedy <nav.bugs.thepoet@gmail.com>
2018-03-11 09:40:48 -0700

    Enchancement 92: Changed the secret key.

--- mainsite/mainsite/settings.py
+++ mainsite/mainsite/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
+from mainsite.keys import *
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'oj54jozx0vi7a501v3tz9@gh)wdfq8!2ko5g0ko32$e^q6s@nd'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
