46c4ea3daed775c5c1f62ec3fcac31da9e4de4fa
Chauncey <gottingen999@gmail.com>
2018-03-14 05:47:32 +0800

    Task#95 Back-End: generation of data for graphs based upon academic areas Status: closed Description: All set. I return a dictionary which indicates the number of every single tag to front-end so that the front-end development can do the graph based on the data I offer.

--- mainsite/stats/views.py
+++ mainsite/stats/views.py
@@ -1,6 +1,12 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from homeapp.models import PDF
+from collections import Counter
+from django.db.models import Count, Func, F
+
 
 # Create your views here.
 def index(request):
-    return HttpResponse("This is indeed working")
+    abc=PDF.objects.annotate(list=Func(F('pdf_tags'), function='unnest')).values_list('list', flat=True)
+    print(Counter(abc))
+    return Counter(abc)
