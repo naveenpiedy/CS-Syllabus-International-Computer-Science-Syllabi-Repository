6a308478d3bc974b2d24d1f8c48dbdc026ab4ebb
Naveen Piedy <nav.bugs.thepoet@gmail.com>
2018-03-16 10:33:20 -0700

    Task number 107: Created a dropdown list for year.

--- mainsite/stats/views.py
+++ mainsite/stats/views.py
@@ -16,12 +16,12 @@ def index(request):
     for i in range(0, len(list_tags), 2):
         list_dic[list_tags[i]] = list_tags[i+1]
     # print(list_dic)
-    print(json.dumps(list(list_dic.values())))
+    # print(json.dumps(list(list_dic.values())))
     chart_setup = {
         'chart_name': 'syllabi',
         'chart_type': 'pie',
-        'chart_labels': list(list_dic.keys()) ,
-        'options': 'options',
+        'chart_labels': list(list_dic.keys()),
+        'options': "options",
         'datasets': {
             'data1': list(list_dic.values()),
             #'data1': [1,2,3,4,5]
