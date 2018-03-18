9ce0aef43b07f68415cf23636b329db65424ab3c
Naveen Piedy <nav.bugs.thepoet@gmail.com>
2018-03-18 12:43:21 -0700

    Enhancement 110: Moved the credentials to another file.

--- mainsite/mainsite/settings.py
+++ mainsite/mainsite/settings.py
@@ -90,16 +90,6 @@ WSGI_APPLICATION = 'mainsite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'SER517',
-        'USER': 'npiedy',
-        'PASSWORD': 'root',
-        'HOST': 'localhost',
-        'PORT': '5432',
-    }
-}
 
 
 # Password validation
