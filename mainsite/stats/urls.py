955f386eba63266d1750ad38837b4eb35a6abfde
Chauncey <gottingen999@gmail.com>
2018-03-21 02:38:22 +0800

    Task#94 Back-end - generation of data for graphs based upon university and level. Status: closed Description: All set guys. When a user gives his input(university or level or both) on website, I can generate a dictionary whose keys are academic areas held in this university&level and whose values are the count showing how many courses held in this university&level. So Naveen can use these data to draw the chart.

--- mainsite/stats/urls.py
+++ mainsite/stats/urls.py
@@ -5,4 +5,5 @@ from . import views
 app_name = 'stats'
 urlpatterns = [
     path('', views.index, name='stats_page'),
+    path('analyze',views.analyze,name='analyze')
 ]
--- mainsite/stats/views.py
+++ mainsite/stats/views.py
@@ -2,6 +2,7 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from django.template.context_processors import csrf
 from homeapp.models import PDF
+from homeapp.models import Tag
 from collections import Counter
 from django.db.models import Count, Func, F
 from mainsite import settings
@@ -80,10 +81,12 @@ def index(request):
     c.update(csrf(request))
     abc = PDF.objects.annotate(list=Func(F('pdf_tags'), function='unnest')).values_list('list', flat=True).annotate(
         num=Count('list'))
+
     list_tags = list(abc)
     list_dic = {}
     for i in range(0, len(list_tags), 2):
         list_dic[list_tags[i]] = list_tags[i + 1]
+
     pie = PieChart()
     bar = BarChart()
     if len(list_dic) > 20:
@@ -97,3 +100,45 @@ def index(request):
         bar.geting_local_data(list(list_dic.keys()), list(list_dic.values()))
     # print(list_dic)
     return render(request, 'stats/overall.html', {'piechart': pie, 'barchart':bar}, c)
+
+def analyze(request):
+    c = {}
+    c.update(csrf(request))
+    print(request.POST)
+    search_lev=''
+    if request.POST['year'] == 'Freshman':
+        search_lev = 'Freshman'
+    if request.POST['year'] == 'Junior':
+        search_lev = 'Junior'
+    if request.POST['year'] == 'Masters':
+        search_lev = 'Masters'
+    if request.POST['year'] == 'Doctorate':
+        search_lev = 'Doctorate'
+    if request.POST['year'] == 'Senior':
+        search_lev = 'Senior'
+    if request.POST['year'] == 'Sophomore':
+        search_lev = 'Sophomore'
+    if request.POST['year'] == 'None':
+        search_lev= 'None'
+    search_uni=request.POST['University']
+
+    if search_uni=='' and search_lev=='None':
+        abc = PDF.objects.annotate(list=Func(F('pdf_tags'), function='unnest')).values_list('list', flat=True).annotate(num=Count('list'))
+    elif search_uni=='' and search_lev!='None':
+        abc = PDF.objects.annotate(list=Func(F('pdf_tags'), function='unnest')).values_list('list', flat=True).filter(
+            year=search_lev).annotate(num=Count('list'))
+    elif search_uni!='' and search_lev=='None':
+        abc = PDF.objects.annotate(list=Func(F('pdf_tags'), function='unnest')).values_list('list', flat=True).filter(
+            university=search_uni).annotate(num=Count('list'))
+    else:
+        abc = PDF.objects.annotate(list=Func(F('pdf_tags'), function='unnest')).values_list('list', flat=True).filter(
+            university=search_uni,year=search_lev).annotate(num=Count('list'))
+    list_tags = list(abc)
+    list_dic = {}
+    for i in range(0, len(list_tags), 2):
+        list_dic[list_tags[i]] = list_tags[i + 1]
+    print(list_dic)
+
+
+
+    return render(request,'stats/universityStats.html',c)
