73a243d8895bb34f01978e8ef4aaad30c03410bf
Nayan Tiwari <nayan.tiwari@asu.edu>
2018-04-09 18:48:32 -0700

    Task #125 - Creating test cases for Loginapp (automated testing) Part-2

--- mainsite/loginapp/tests.py
+++ mainsite/loginapp/tests.py
@@ -1,45 +1,96 @@
 from django.contrib.auth.models import User
 from django.test import TestCase
 
+
 # Create your tests here.
 class LoginTest(TestCase):
     def setUp(self):
-        self.credentials1 = {'username' : 'test1username',
-                            'password' : 'test1password'}
+        self.userinfo1 = {'username': 'test1username',
+                          'password': 'test1password',
+                          'first_name': 'test1first_name',
+                          'last_name': 'test1last_name',
+                          'email': 'test1email@test.edu',
+                          'university': 'ASU',
+                          'isprofessor': 'True'}
 
-        User.objects.create_user(**self.credentials1)
+        self.credentials1 = {'username': 'test1username',
+                             'password': 'test1password'}
+
+        User.objects.create_user(**self.userinfo1)
+
+        self.userinfo2 = {'username': 'test2username',
+                          'password': 'test2password',
+                          'first_name': 'test2first_name',
+                          'last_name': 'test2last_name',
+                          'email': 'test2email@test.edu',
+                          'university': 'ASU',
+                          'isprofessor': 'True'}
 
         self.credentials2 = {'username': 'test2username',
-                            'password': 'test2password'}
+                             'password': 'test2password'}
 
-        User.objects.create_user(**self.credentials2)
+        User.objects.create_user(**self.userinfo2)
+        self.userinfo3 = {'username': 'test3username',
+                          'password': 'test3password',
+                          'first_name': 'test3first_name',
+                          'last_name': 'test3last_name',
+                          'email': 'test3email@test.edu',
+                          'university': 'ASU',
+                          'isprofessor': 'True'}
+
         self.credentials3 = {'username': 'test3username',
-                            'password': 'test3password'}
+                             'password': 'test3password'}
 
-        User.objects.create_user(**self.credentials3)
+        User.objects.create_user(**self.userinfo3)
+
+        self.userinfo4 = {'username': 'test4username',
+                          'password': 'test4password',
+                          'first_name': 'test4first_name',
+                          'last_name': 'test4last_name',
+                          'email': 'test4email@test.edu',
+                          'university': 'ASU',
+                          'isprofessor': 'True'}
 
         self.credentials4 = {'username': 'test4username',
-                            'password': 'test4password'}
+                             'password': 'test4password'}
 
-        User.objects.create_user(**self.credentials4)
+        User.objects.create_user(**self.userinfo4)
+
+        self.userinfo5 = {'username': 'test5username',
+                          'password': 'test5password',
+                          'first_name': 'test5first_name',
+                          'last_name': 'test5last_name',
+                          'email': 'test5email@test.edu',
+                          'university': 'ASU',
+                          'isprofessor': 'True'}
 
         self.credentials5 = {'username': 'test5username',
-                            'password': 'test5password'}
+                             'password': 'test5password'}
 
-        User.objects.create_user(**self.credentials5)
+        User.objects.create_user(**self.userinfo5)
 
     def test_login(self):
-        response = self.client.post('/login/', self.credentials1, follow = True)
+        self.client.login(**self.credentials1)
+        response = self.client.post('/login/', self.userinfo1, follow=True)
         self.assertTrue(response.context['user'].is_active)
+        self.client.logout()
 
-        response = self.client.post('/login/', self.credentials2, follow=True)
+        self.client.login(**self.credentials2)
+        response = self.client.post('/login/', self.userinfo2, follow=True)
         self.assertTrue(response.context['user'].is_active)
+        self.client.logout()
 
-        response = self.client.post('/login/', self.credentials3, follow=True)
+        self.client.login(**self.credentials3)
+        response = self.client.post('/login/', self.userinfo3, follow=True)
         self.assertTrue(response.context['user'].is_active)
+        self.client.logout()
 
-        response = self.client.post('/login/', self.credentials4, follow=True)
+        self.client.login(**self.credentials4)
+        response = self.client.post('/login/', self.userinfo4, follow=True)
         self.assertTrue(response.context['user'].is_active)
+        self.client.logout()
 
-        response = self.client.post('/login/', self.credentials5, follow=True)
+        self.client.login(**self.credentials5)
+        response = self.client.post('/login/', self.userinfo5, follow=True)
         self.assertTrue(response.context['user'].is_active)
+        self.client.logout()
