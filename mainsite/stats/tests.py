84da45dc999cab602dce616c1118a4e5b3587941
Chauncey <gottingen999@gmail.com>
2018-04-11 06:47:04 +0800

    Task#166 Stats-app automate test case for uni_analysis view method Status: Closed Description: The university analysis view method in stats-app is valid and verified.

--- mainsite/stats/tests.py
+++ mainsite/stats/tests.py
@@ -70,3 +70,45 @@ class IndexTest2(TestCase):
             university='ASU', year='Junior').annotate(num=Count('list'))
         list_tags = list(abc)
         self.assertEqual(list_tags[0], 'Software Engineering')
+
+class IndexTest3(TestCase):
+    def setUp(self):
+        User.objects.create_user("Test1", email='test1@test.edu', password='test1password', first_name='Test',
+                                 last_name='1')
+        UserTable.objects.create(user=User.objects.get(username='Test1'), university="ASU", isprofessor=True)
+        PDF.objects.create(uploaders='Test1', university='ASU', pdfName='pdf1', professor_name='Prof.Chen',
+                           subjectName='SER518',year='Senior', pdf_desc='desc1', pdf_topic='topic1', pdf_tags=['Software Engineering'])
+        PDF.objects.create(uploaders='Test2', university='ASU', pdfName='pdf2', professor_name='Prof.Zhang',
+                           subjectName='SER519',year='Junior', pdf_desc='desc2', pdf_topic='topic2', pdf_tags=['Software Engineering'])
+        PDF.objects.create(uploaders='Test1', university='OSU', pdfName='pdf2', professor_name='Prof.Zhang',
+                           subjectName='SER519', year='Junior', pdf_desc='desc2', pdf_topic='topic2',
+                           pdf_tags=['Software Engineering'])
+        PDF.objects.create(uploaders='Test2', university='NCSU', pdfName='pdf2', professor_name='Prof.Zhang',
+                           subjectName='SER519', year='Junior', pdf_desc='desc2', pdf_topic='topic2',
+                           pdf_tags=['Software Engineering'])
+        PDF.objects.create(uploaders='Test2', university='NCSU', pdfName='pdf2', professor_name='Prof.Zhang',
+                           subjectName='SER519', year='Junior', pdf_desc='desc2', pdf_topic='topic2',
+                           pdf_tags=['Software Engineering'])
+        PDF.objects.create(uploaders='Test2', university='Harvard', pdfName='pdf2', professor_name='Prof.Zhang',
+                           subjectName='SER519', year='Junior', pdf_desc='desc2', pdf_topic='topic2',
+                           pdf_tags=['Software Engineering'])
+        PDF.objects.create(uploaders='Test2', university='MIT', pdfName='pdf2', professor_name='Prof.Zhang',
+                           subjectName='SER519', year='Junior', pdf_desc='desc2', pdf_topic='topic2',
+                           pdf_tags=['Software Engineering'])
+
+
+
+
+    def test_check_this_works(self):
+        c = Client()
+        test1 = User.objects.get(username='Test1')
+        self.assertEqual(test1.username, 'Test1')
+        c.login(username='Test1', password='test1password')
+        abc = PDF.objects.filter(pdf_tags__contains=['Software Engineering'])
+        uni_list = []
+        for one_pdf in list(abc):
+            uni_list.append(one_pdf.university)
+
+        dic = Counter(uni_list)
+        print(dic)
+        self.assertEqual(dic, {'ASU': 2, 'NCSU': 2, 'OSU': 1, 'Harvard': 1, 'MIT': 1})
